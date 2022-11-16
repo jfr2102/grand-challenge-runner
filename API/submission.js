e7a0f097092eba331fab4383c870329fc48d1bde
jfr2102 <johannes@friedlein.eu>
2022-11-16 18:23:59 +0100

    [INIT] create POC to upload and save files to express

---
+++ API/submission.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { v4: uuidv4 } = require("uuid");
+
+router.get("/", async (req, res) => {
+  console.log(req.body);
+
+  res.send("You can upload your submission to /submission/upload");
+});
+
+router.post("/upload/", function (req, res) {
+  file = req.files.myfile;
+
+  console.log(file); // the uploaded file object
+
+  console.log;
+
+  file.mv("upload/" + uuidv4() + "docker-stack.yml");
+
+  res.status(200).send("ok");
+});
+
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+
+const app = express();
+
+const cors = require("cors");
+
+const submission = require("./API/submission");
+
+const fileUpload = require("express-fileupload");
+
+const port = 3000;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    createParentPath: true,
+  })
+);
+
+app.use("/submission", submission);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
