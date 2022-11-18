984cd166a0af6357e2157fbe642cd441d13a32ff
jfr2102 <johannes@friedlein.eu>
2022-11-18 22:19:33 +0100

    add example to run unix command

--- API/submission.js
+++ API/submission.js
@@ -1,25 +1,36 @@
 const express = require("express");
+const { exec } = require("node:child_process");
 
 const router = express.Router();
 
 const { v4: uuidv4 } = require("uuid");
+const experiments = [];
 
 router.get("/", async (req, res) => {
-  console.log(req.body);
-
+  console.log("Get /");
   res.send("You can POST your submission to /submission/upload");
 });
 
 router.post("/upload/", function (req, res) {
+  console.log("list:", experiments);
   file = req.files.myfile;
 
   console.log(file); // the uploaded file object
 
-  console.log;
+  experiments.push(file.name);
 
   file.mv("upload/" + uuidv4() + "docker-stack.yml");
 
   res.status(200).send("ok");
 });
 
+const deploySwarm = () => {
+  exec("docker ps -a", (err, output) => {
+    if (err) {
+      console.error("could not execute command: ", err);
+      return;
+    }
+    console.log("Output: \n", output);
+  });
+};
 module.exports = router;
