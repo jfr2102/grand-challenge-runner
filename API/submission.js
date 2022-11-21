792159aae2ab8b63b70ee63f9372e6848afe9a14
Johannes Friedlein <johannes@friedlein.eu>
2022-11-21 18:53:03 +0100

    deploy ocker swarm / local docker compose after upload

--- API/submission.js
+++ API/submission.js
@@ -1,36 +1,35 @@
 const express = require("express");
 const { exec } = require("node:child_process");
-
 const router = express.Router();
 
 const { v4: uuidv4 } = require("uuid");
 const experiments = [];
 
 router.get("/", async (req, res) => {
-  console.log("Get /");
-  res.send("You can POST your submission to /submission/upload");
+  res.send("/submission endpoint")
 });
 
 router.post("/upload/", function (req, res) {
   console.log("list:", experiments);
   file = req.files.myfile;
-
-  console.log(file); // the uploaded file object
-
+  console.log(file);
   experiments.push(file.name);
-
-  file.mv("upload/" + uuidv4() + "docker-stack.yml");
-
+  const fileName = `upload/${uuidv4()}docker-stack.yml`
+  file.mv(fileName);
+  runExperiment(fileName)
   res.status(200).send("ok");
 });
 
-const deploySwarm = () => {
-  exec("docker ps -a", (err, output) => {
-    if (err) {
-      console.error("could not execute command: ", err);
-      return;
-    }
-    console.log("Output: \n", output);
-  });
-};
+const runExperiment = async (compose_file) => {
+  // exec(`docker stack deploy --compose-file ${compose_file} benchmarkApp`, (err, output) => {
+    exec(`docker-compose -f ${compose_file} up`, (err, output) => {
+      if (err) {
+        console.error("could not execute command: ", err);
+        return err;
+      }
+      console.log("Output: \n", output);
+    });
+}
+
+
 module.exports = router;
