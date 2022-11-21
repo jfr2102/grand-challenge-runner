70b5c12d49d16dd910172e984e2248d447334f37
jfr2102 <johannes@friedlein.eu>
2022-11-21 23:52:09 +0100

    change child_process require

--- API/submission.js
+++ API/submission.js
@@ -1,12 +1,13 @@
 const express = require("express");
-const { exec } = require("node:child_process");
+const { exec } = require("child_process");
+
 const router = express.Router();
 
 const { v4: uuidv4 } = require("uuid");
 const experiments = [];
 
 router.get("/", async (req, res) => {
-  res.send("/submission endpoint")
+  res.send("/submission endpoint");
 });
 
 router.post("/upload/", function (req, res) {
@@ -14,22 +15,21 @@ router.post("/upload/", function (req, res) {
   file = req.files.myfile;
   console.log(file);
   experiments.push(file.name);
-  const fileName = `upload/${uuidv4()}docker-stack.yml`
+  const fileName = `upload/${uuidv4()}docker-stack.yml`;
   file.mv(fileName);
-  runExperiment(fileName)
+  runExperiment(fileName);
   res.status(200).send("ok");
 });
 
 const runExperiment = async (compose_file) => {
   // exec(`docker stack deploy --compose-file ${compose_file} benchmarkApp`, (err, output) => {
-    exec(`docker-compose -f ${compose_file} up`, (err, output) => {
-      if (err) {
-        console.error("could not execute command: ", err);
-        return err;
-      }
-      console.log("Output: \n", output);
-    });
-}
-
+  exec(`docker-compose -f ${compose_file} up`, (err, output) => {
+    if (err) {
+      console.error("could not execute command: ", err);
+      return err;
+    }
+    console.log("Output: \n", output);
+  });
+};
 
 module.exports = router;
