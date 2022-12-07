a145ca4359df8f2d9ec7ffd30f9106aabda8e4d4
jfr2102 <johannes@friedlein.eu>
2022-12-07 02:27:33 +0100

    merge

--- controller/experimentController.js
+++ controller/experimentController.js
@@ -3,13 +3,13 @@ const { exec } = require("child_process");
 const runExperiment = async (compose_file) => {
   if (process.env.ENV === "dev") {
     console.log("DEV, dont run command");
-    exec(`docker-compose -f ${compose_file} up`, (err, output) => {
-      if (err) {
-        console.error("could not execute command: ", err);
-        return err;
-      }
-      console.log("Output: \n", output);
-    });
+    // exec(`docker-compose -f ${compose_file} up`, (err, output) => {
+    //   if (err) {
+    //     console.error("could not execute command: ", err);
+    //     return err;
+    //   }
+    //   console.log("Output: \n", output);
+    // });
   } else {
     exec(`docker stack deploy --compose-file ${compose_file} benchmarkApp`, (err, output) => {
       if (err) {
