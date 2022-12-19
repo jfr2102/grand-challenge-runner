f98b874cb405d2943dfeea0b5c15f56a1fde79c4
Johannes Friedlein <48349638+jfr2102@users.noreply.github.com>
2022-12-20 00:47:20 +0100

    Test deployment Kill worker and remove stack (#2)
    * delete stack endpoint
    
    * shorten submission ids to prevent docker error
    
    * start worker kill implementation
    
    * get swarm host ip that runs a service

--- controller/experimentController.js
+++ controller/experimentController.js
@@ -1,6 +1,7 @@
 const { exec } = require("child_process");
 
-const runExperiment = async (compose_file, id) => {
+const runExperiment = async (compose_file, id, labels) => {
+  console.log(id);
   if (process.env.ENV === "dev") {
     console.log("DEV, dont run command");
     // exec(`docker-compose -f ${compose_file} up`, (err, output) => {
@@ -12,7 +13,7 @@ const runExperiment = async (compose_file, id) => {
     // });
   } else {
     var error;
-    console.log("Deploy " + compose_file);
+    console.log("Deploy " + compose_file + "with id: " + id + "\n");
     exec(`docker stack deploy --compose-file ${compose_file} submission_${id}`, (err, output) => {
       if (err) {
         console.error("could not execute command: ", err);
@@ -24,6 +25,70 @@ const runExperiment = async (compose_file, id) => {
   }
 };
 
+/**
+ * Kill a random worker of a submission stack
+ * @param {*} id - submission ID of the stack
+ * @param {*} workerServices - service name list of all worker services
+ */
+
+function get_random(list) {
+  return list[Math.floor(Math.random() * list.length)];
+}
+
+/**
+ *
+ * @param {*} id
+ * @param {*} workerServices
+ */
+const killOneWorker = (id, workerServices) => {
+  // pick a random one of the worker services (if there are several)
+  const serviceToKill = get_random(workerServices);
+  const fullServiceName = `submission_${id}_${serviceToKill}`;
+  // we want to find the IP address of one of the swarm nodes the service is running on
+  // first list nodes this service is running one
+  var swarmNodes = [];
+  var hostIP;
+  exec(`docker service ps ${fullServiceName} --format "{{.Node}}"`, (err, output) => {
+    // exec(`ls -l`, (err, output) => {
+    if (err) {
+      console.error("could not execute command: ", err);
+    }
+    console.log("Output: \n", output);
+
+    output.split("\n").forEach((line) => {
+      swarmNodes.push(line.replace(/\s/g, ""));
+    });
+
+    //choose random swarm worker node that runs an instance of this service:
+    const workerHost = get_random(swarmNodes);
+    exec(`docker node inspect ${workerHost} --format '{{.Status.Addr}}'`, (err, output) => {
+      if (err) {
+        console.error("could not execute command: ", err);
+      }
+      hostIP = output;
+      console.log("Output: \n", output);
+    });
+  });
+
+  console.log("HOST IP: ", hostIP);
+  // now we need to send this host a message to kill one of the service instances that he is running
+};
+
+const removeStack = (id) => {
+  console.log(`removing stack: submission_${id}`);
+  var error;
+  exec(`docker stack rm submission_${id}`, (err, output) => {
+    if (err) {
+      console.error("could not execute command: ", err);
+      error = err;
+    }
+    console.log("Output: \n", output);
+  });
+  return error ? true : false;
+};
+
 module.exports = {
   runExperiment,
+  killOneWorker,
+  removeStack,
 };
